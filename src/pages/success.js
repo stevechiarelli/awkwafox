deb0f240094cda62a6b679bfd3139638b42962eb
Steve Chiarelli <spchiare@yahoo.com>
2021-08-29 15:22:19 -0400

    fix layout import

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { graphql } from "gatsby";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import Hero from "../components/Hero";
 import Modal from "../components/Modal";
 import Seo from "../components/Seo";
--- src/pages/webdesign.js
+++ src/pages/webdesign.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { graphql } from "gatsby";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import Info from "../components/Info";
