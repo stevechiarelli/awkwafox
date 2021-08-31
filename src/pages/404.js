0652f28b6ba5510e7586415a8b1a8dcbd69d6d77
Steve Chiarelli <spchiare@yahoo.com>
2021-08-31 16:14:35 -0400

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,43 +1,54 @@
 import React from "react";
+import Layout from "../components/Layout";
 import { Link } from "gatsby";
 import styled from "styled-components";
 
 const NotFoundPage = () => {
     return (
-        <Wrapper>
-            <div class="container">
-                <h1> 404</h1>
-                <h2>Oops.. You just found an error page...</h2>
-                <p>We're sorry but the page you requested was not found.</p>
-                <Link to="/" className="btn-primary">return home</Link>
-            </div>
-        </Wrapper>
+        <Layout page="error">
+            <Wrapper>
+                <div className="container">
+                    <h1> 404</h1>
+                    <h2>Oops.. You just found an error page...</h2>
+                    <p>We're sorry but the page you requested was not found.</p>
+                    <Link to="/" className="btn-primary">return home</Link>
+                </div>
+            </Wrapper>
+        </Layout>
     )
 }
 
 const Wrapper = styled.section`
+    background: var(--background6);
     display: flex;
     align-items: center;
     height: 100vh;
+    width: 100%;
     text-align: center;
 
     h1 {
         font-size: 3em;
         font-weight: 400;
-        color: #9aa0ac;
-    }
-
-    p {
-        color: #9aa0ac;
+        color: #ccc;
     }
 
     h2 {
-        color: var(--primary);
+        color: var(--text-light);
         font-weight: 600;
     }
 
+    p {
+        color: var(--text-light);
+        margin-bottom: 2em;
+    }
+
+    .btn-primary {
+        color: var(--text-light);
+        border: 3px solid var(--text-light);
+    }
+
     .btn-primary:hover {
-        background: #fff;
+        background: var(--primary-dark);
     }
 `
 
