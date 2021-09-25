2288cb873f9d2732fa8a0e4982b84f8d817783db
Steve Chiarelli <spchiare@yahoo.com>
2021-09-24 23:26:48 -0400

    Contact form update

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -148,6 +148,19 @@ const ContactForm = (props) => {
                                     required={state.JobType === "Wedding" || state.JobType === "Live Stream" ? "required" : ""}
                                 />
                             </div>
+                            <div className={state.JobType === undefined ? "form-group hidden" : "form-group"}>>
+                                <label htmlFor="Source">How did you hear about us?</label><br />
+                                <select id="Source" onChange={handleChange} required>
+                                    <option hidden value=""> -- select an option -- </option>
+                                    <option value="Google">Google</option>
+                                    <option value="Facebook">Facebook</option>
+                                    <option value="The Knot">The Knot</option>
+                                    <option value="Wedding Wire">Wedding Wire</option>
+                                    <option value="Vendor Referral">Vendor Referral</option>
+                                    <option value="Client Referral">Client Referral</option>
+                                    <option value="Other">Other</option>
+                                </select>
+                            </div>
                             <div className="form-group">
                                 <label htmlFor="Message">Message</label><br />
                                 <textarea 
